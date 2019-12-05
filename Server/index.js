7f2967be6a2dd44e1ea38c9fa890f6df6d8f053f
Sergii Akimov <sergii.akimov@silksh.pl>
2019-12-05 15:56:18 +0100

    Composer

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,12 @@
-const dotenv = require('dotenv').config();
 const server = require('./bundles/server');
 const io = require('socket.io')(server);
 const redis = require("redis");
-const subscriber = redis.createClient(JSON.parse(dotenv.parsed.SOCKET_IO_SUB));
-const publisher = redis.createClient(JSON.parse(dotenv.parsed.SOCKET_IO_PUB));
+const subscriber = redis.createClient(JSON.parse(process.env.SOCKET_IO_SUB));
+const publisher = redis.createClient(JSON.parse(process.env.SOCKET_IO_PUB));
 const RedisIO = require('./bundles/redis-io');
 const axios = require('axios');
 
-(new RedisIO(dotenv.parsed.SOCKET_IO_NSP, io, subscriber, publisher, dotenv.parsed.SOCKET_IO_CHANNELS.split(',')))
+(new RedisIO(process.env.SOCKET_IO_NSP, io, subscriber, publisher, process.env.SOCKET_IO_CHANNELS.split(',')))
     .listen();
 
 const authMiddleware = (function (socket, next) {
@@ -15,21 +14,29 @@ const authMiddleware = (function (socket, next) {
         next(new Error('Token requied'));
     }
 
+    console.log('Auth:', socket.handshake.query.token);
+
     axios.post(
         process.env.SOCKET_IO_VALIDATE_TOKEN,
         {},
         {headers: {Authorization: "Bearer " + socket.handshake.query.token}}
     )
         .then(response => {
+            console.log('Auth success', socket.handshake.query.token, response);
             socket.handshake.user_id = response.data.user_id;
             next();
         })
-        .catch(error => next(error));
+        .catch(error => {
+            console.error('Auth error', socket.handshake.query.token, error);
+            next(error)
+        });
 });
 
 io.of('notifications').use(authMiddleware);
 
 server.listen(
-    dotenv.parsed.SOCKET_IO_WS_SERVER.split(':')[1],
-    dotenv.parsed.SOCKET_IO_WS_SERVER.split(':')[0]
+    process.env.SOCKET_IO_WS_SERVER.split(':')[1],
+    process.env.SOCKET_IO_WS_SERVER.split(':')[0]
 );
+
+console.log('Listen');
